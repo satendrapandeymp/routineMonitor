fc08fec89df94d9e035637bd355115af82b1aece
Satyendra Pandey <satyendrapandeyiitk@gmail.com>
2019-10-19 06:33:27 +0530

    Stable v 0.0.1

--- todo.js
+++ todo.js
@@ -1,21 +1,29 @@
-function dragStart(event) {
-  event.dataTransfer.setData("Text", event.target.id);
-  document.getElementById("i1").style.borderColor= "#0000FF";
-  document.getElementById("i1").style.borderWidth= "4px";
-  document.getElementById("i2").style.borderColor= "#0000FF";
-  document.getElementById("i2").style.borderWidth= "4px";
+
+function addNewActivity(event)
+{
+    if (event.keyCode == 13)
+    {
+        var text = document.getElementById("addActivity").value;
+        var childLength = document.getElementById("myActivityList").childNodes.length;
+        var activity = new newActivity(text, childLengths);
+        document.getElementById("myActivityList").appendChild(activity.activity);
+    }
 }
 
-function dragging(event, par) {
-  par.style.fontcolor = "FF0000";
+function dragStart(event) {
+  event.dataTransfer.setData("sourceId", event.target.id);
+  document.getElementById("completedTask").style.borderColor= "#0000FF";
+  document.getElementById("completedTask").style.borderWidth= "5px";
+  document.getElementById("pendingTask").style.borderColor= "#0000FF";
+  document.getElementById("pendingTask").style.borderWidth= "5px";
 }
 
 function dragend(event, par) {
   console.log(par);
-  document.getElementById("i1").style.borderColor= "#000000";
-  document.getElementById("i1").style.borderWidth= "1px";
-  document.getElementById("i2").style.borderColor= "#000000";
-  document.getElementById("i2").style.borderWidth= "1px";      
+  document.getElementById("completedTask").style.borderColor= "#000000";
+  document.getElementById("completedTask").style.borderWidth= "3px";
+  document.getElementById("pendingTask").style.borderColor= "#000000";
+  document.getElementById("pendingTask").style.borderWidth= "3px";      
 }
 
 function allowDrop(event, par) {
@@ -29,7 +37,7 @@ function disallowDrop(event, par) {
 
 function drop(event) {
   event.preventDefault();
-  var data = event.dataTransfer.getData("Text");
+  var data = event.dataTransfer.getData("sourceId");
   var row1 = document.getElementById(data);
   var row = row1.cloneNode(true);
   row1.remove();
@@ -37,28 +45,36 @@ function drop(event) {
   //event.target.appendChild(document.getElementById(data));
 //   var li = document.createElement("li");
 //   li.appendChild(row);
-  event.target.firstChild.nextSibling.appendChild(row);
+  event.target.firstChild.nextSibling.nextSibling.nextSibling.appendChild(row);
 }
 
-function add(event)
+function addNewActivity(event)
 {
     if (event.keyCode == 13)
     {
-        var text = document.getElementById("act").value;
-        var childLength = document.getElementById("myList").childNodes.length;
-        var activity = new newActivity(text, childLength);
-        document.getElementById("myList").appendChild(activity.activity);
+        var activity = new newActivity();
+        document.getElementById("myActivityList").appendChild(activity.activity);
     }
 }
 
+function deleteActivity(activity) {
+  activity.remove();
+}
+
 class newActivity {
-    constructor(text, childLength)
+    constructor()
     {
-        this.text = text;
-        this.childLength = childLength;
-        this.createElement();
+        this.getData();
         this.createActivity();
+        this.createElements();
         this.setId();
+    }
+
+    getData() {
+      var taskName = document.getElementById("taskName").value;
+      var taskDesc = document.getElementById("taskDesc").value;
+      var taskData = document.getElementById("taskData").value;
+      this.data = {"taskName": taskName, "taskDesc": taskDesc, "taskData": taskData};
     }
 
     guidGenerator() {
@@ -68,18 +84,23 @@ class newActivity {
         return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
     }
 
-    createElement() {
-        var activity = document.createElement("p");
-        activity.innerText = this.text;
-        this.element = activity;
-    }
+    createElements() {
+        var activityTaskName = document.createElement("td");
+        activityTaskName.innerText = this.data.taskName;
+        this.activity.appendChild(activityTaskName);
+        var activityTaskDesc = document.createElement("td");
+        activityTaskDesc.innerText = this.data.taskDesc;
+        this.activity.appendChild(activityTaskDesc);
+        var activityTaskData = document.createElement("td");
+        activityTaskData.innerText = this.data.taskData;
+        this.activity.appendChild(activityTaskData);
+      }
 
     createActivity() {
-        var activity = document.createElement("li");
-        activity.appendChild(this.element);
+        var activity = document.createElement("tr");
         activity.setAttribute("ondragstart", "dragStart(event)");
-        activity.setAttribute("ondrag", "dragging(event, this)");
         activity.setAttribute("ondragend", "dragend(event, this)");
+        activity.setAttribute("ondblclick", "deleteActivity(this)");
         activity.setAttribute("draggable", "true");
         this.activity = activity;
     }
@@ -89,6 +110,7 @@ class newActivity {
         this.activity.setAttribute("id", id);
     }
 }
+
 
 addEventListener("keydown", function(event) {
 if (event.keyCode == 27)
